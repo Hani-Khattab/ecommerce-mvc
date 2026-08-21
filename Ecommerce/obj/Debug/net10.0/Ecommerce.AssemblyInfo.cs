fa2457032d08841b2575fa11b80134d11f6858bc
Hani-Khattab <hanikhattab266@gmail.com>
2026-08-21 19:18:16 +0300

    add partial in User Area

--- Ecommerce/obj/Debug/net10.0/Ecommerce.AssemblyInfo.cs
+++ Ecommerce/obj/Debug/net10.0/Ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75ee9b366bc4c04bd59898fa0ebcc8b19b9623a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
